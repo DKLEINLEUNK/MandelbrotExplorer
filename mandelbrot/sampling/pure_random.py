04b8e68365d5101d0e1905a65d3d2268e11490da
DKLEINLEUNK <d.kleinleunk@gmail.com>
2023-11-16 00:09:07 +0100

    Optimized pure random sampler based on orthogonal.py

--- mandelbrot/sampling/pure_random.py
+++ mandelbrot/sampling/pure_random.py
@@ -1,34 +1,33 @@
 import numpy as np
 
+from ..Mandelbrot import Mandelbrot
 from ..plotter import plot_mandbrot_sample
-from ..utils import in_mandelbrot
+from ..utils import in_mandelbrot_vectorized
 
-def sampler(grid: np.ndarray, n_samples=1000, max_iter=256, plot=False, verbose=False):
-    '''Monte Carlo integration with `n_samples` pure random samples to estimate the area of `grid`.
 
-    :param grid: grid of complex numbers
+def sampler(mandelbrot: Mandelbrot, n_samples=1000, max_iter=256, plot=False, verbose=False):
+    '''Performs Monte Carlo sampling on a grid of complex numbers.
 
+    :param mandelbrot: Mandelbrot set
     :param n_samples: number of samples to take
+    :param max_iter: maximum number of iterations to perform
+    :param plot: whether to plot the sample
+    :param verbose: whether to print out information about the sampling process
 
-    :param max_iter: maximum number of iterations
-
-    :param plot: if True, plots the sampled Mandelbrot set
-
-    :return: `sample`, `is_in_mandelbrot` -> pure random sample and boolean array indicating whether each point in the sample lies within the Mandelbrot set
+    :return: a sample of complex numbers and a boolean array indicating whether each point in the sample lies within the Mandelbrot set
     '''
-    flat_grid = grid.flatten()  # 1D array of complex numbers
+    # 1. Flatten the grid of the mandelbrot set:
+    flat_grid = mandelbrot.grid.flatten()
 
-    # Select n_samples indices at random from the grid
+    # 2. Randomly select n_samples indices from the grid:
     indices = np.random.choice(flat_grid.size, size=n_samples, replace=False)
     sample = flat_grid[indices]
 
-    # Iterate each point in the sample, determine whether it lies within the Mandelbrot set
-    is_in_mandelbrot = np.zeros(n_samples, dtype=bool)
-    for i in range(len(sample)):
-        is_in_mandelbrot[i] = in_mandelbrot(sample[i], MAX_ITER=max_iter)
+    # 3. Determine whether the samples lie within the Mandelbrot set:
+    is_in_mandelbrot = in_mandelbrot_vectorized(sample, MAX_ITER=max_iter)
 
     if verbose:
-        print(f'Grid size: {grid.size}')
+        print(f'Grid size: {mandelbrot.grid.size}')
         print(f'Flattened grid size: {flat_grid.size}')
         print(f'#Indices: {indices.size}')
         print(f'Indices: {indices}')
@@ -36,6 +35,6 @@ def sampler(grid: np.ndarray, n_samples=1000, max_iter=256, plot=False, verbose=
         print(f'\nNumber of points in the sample that lie within the Mandelbrot set: {np.sum(is_in_mandelbrot)}')
 
     if plot:
-        plot_mandbrot_sample(sample, is_in_mandelbrot, plot_color='purple')
+        plot_mandbrot_sample(sample, is_in_mandelbrot, plot_color='b')
 
     return sample, is_in_mandelbrot
